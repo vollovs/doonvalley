41c49f97e7f1d37272def82a2a65dea984e47061
dustin <dike.zhang@gmail.com>
2015-08-11 21:57:12 -0400

    update url

--- django/esite/urls.py
+++ django/esite/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-import page.urls
+import page.urls, member.urls
 from page import views as pageview
 
 urlpatterns = [
